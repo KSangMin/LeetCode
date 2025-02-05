5b6b17379b113eb829551349e428024357580ce2
KSangMin <fhfpszmfnwm@naver.com>
2025-02-05 15:51:10 +0900

    [733] Flood Fill

---
+++ 733/Solution.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    public void BFS(ref int[][] image, int sX, int sY, int color)
+    {
+        int[] dx = { 1, -1, 0, 0 };
+        int[] dy = { 0, 0, 1, -1 };
+        bool[,] visited = new bool[image.Length, image[0].Length];
+        Queue<Tuple<int, int>> q = new Queue<Tuple<int, int>>();
+
+        q.Enqueue(new Tuple<int, int>(sX, sY));
+        visited[sX, sY] = true;
+        int sColor = image[sX][sY];
+        image[sX][sY] = color;
+        while(q.Count > 0)
+        {
+            int cX = q.Peek().Item1;
+            int cY = q.Peek().Item2;
+            q.Dequeue();
+
+            for(int i = 0; i < 4; i++)
+            {
+                int nX = cX + dx[i];
+                int nY = cY + dy[i];
+
+                if (0 <= nX && nX < image.Length
+                    && 0 <= nY && nY < image[0].Length
+                    && visited[nX, nY] == false
+                    && image[nX][nY] == sColor)
+                {
+                    image[nX][nY] = color;
+                    visited[nX, nY] = true;
+                    q.Enqueue(new Tuple<int,int>(nX, nY));
+                }
+            }
+        }
+    }
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        BFS(ref image, sr, sc, color);
+
+        return image;
+    }
+}
