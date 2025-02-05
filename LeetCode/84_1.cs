7956a8bea1f522b6cbc234092b47c1cd46b9161d
KSangMin <fhfpszmfnwm@naver.com>
2025-02-05 14:55:27 +0900

    [84] Largest Rectangle in Histogram

---
+++ LeetCode/84_1.cs
@@ -0,0 +1,30 @@
+﻿public class _84_1
+{
+    public int LargestRectangleArea(int[] heights)
+    {
+        return Find(ref heights, 0, heights.Length - 1);
+    }
+
+    public int Find(ref int[] hist, int left, int right)
+    {
+        if (left == right) return hist[left];
+        int mid = (left + right) / 2;
+        int i = mid, j = mid + 1;
+
+        int minH = Math.Min(hist[i], hist[j]);
+        int ans = minH * 2;
+
+        while (left < i || j < right)
+        {
+            if (i == left && j < right) j++;
+            else if (j == right && left < i) i--;
+            else if (hist[i - 1] < hist[j + 1]) j++;
+            else i--;
+
+            minH = Math.Min(minH, Math.Min(hist[i], hist[j]));
+            ans = Math.Max(ans, minH * (j - i + 1));
+        }
+
+        return Math.Max(ans, Math.Max(Find(ref hist, left, mid), Find(ref hist, mid + 1, right)));
+    }
+}
---
+++ LeetCode/84_2.cs
@@ -0,0 +1,23 @@
+﻿public class _84_2
+{
+    public int LargestRectangleArea(int[] heights)
+    {
+        Stack<int> s = new Stack<int>();
+        int m = 0;
+
+        for(int i = 0; i <= heights.Length; i++)
+        {
+            int h = i < heights.Length ? heights[i] : 0;
+
+            while(s.Any() && heights[s.Peek()] > h)
+            {
+                int minH = heights[s.Pop()];
+                int prevId  = s.Any() ? s.Peek() : -1;
+                m = Math.Max(m, minH * (i - 1 - prevId));
+            }
+            s.Push(i);
+        }
+
+        return m;
+    }
+}
